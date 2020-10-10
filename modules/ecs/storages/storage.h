456c6e5574d670ea267108ff8cd96b1564bdbf66
Andrea Catania <info@andreacatania.com>
2020-10-10 13:06:48 +0200

    Added the storages directory.
    The godot gitignore https://github.com/godotengine/godot/blob/master/.gitignore#L40 is causing that dir to be ignored.

---
+++ modules/ecs/storages/storage.h
@@ -0,0 +1,40 @@
+/* Author: AndreaCatania */
+
+#ifndef STORAGE_H
+#define STORAGE_H
+
+#include "modules/ecs/ecs.h"
+
+enum class StorageType {
+	NONE,
+	DENSE_VECTOR,
+};
+
+class Storage {
+public:
+	virtual StorageType get_type() const { return StorageType::NONE; }
+	virtual String get_type_name() const { return "Overload this function `get_type_name()` please."; }
+	virtual void remove(EntityID p_index) {}
+};
+
+template <class T>
+class TypedStorage : public Storage {
+public:
+	virtual void insert(EntityID p_entity, T p_data) {
+		CRASH_NOW_MSG("Override this function.");
+	}
+
+	virtual bool has(EntityID p_entity) const {
+		CRASH_NOW_MSG("Override this function.");
+	}
+
+	virtual const T &get(EntityID p_entity) const {
+		CRASH_NOW_MSG("Override this function.");
+	}
+
+	virtual T &get(EntityID p_entity) {
+		CRASH_NOW_MSG("Override this function.");
+	}
+};
+
+#endif
