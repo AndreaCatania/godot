7ee2a7737cdf6e687ea832bea6a60ace8df3c7b1
Andrea Catania <info@andreacatania.com>
2020-10-10 10:19:19 +0200

    added component add and remove into the inspector

--- modules/ecs/editor_plugins/entity_editor_plugin.cpp
+++ modules/ecs/editor_plugins/entity_editor_plugin.cpp
@@ -30,6 +30,7 @@ void EntityEditor::create_editors() {
 
 	add_component_menu = memnew(MenuButton);
 	add_component_menu->set_text("Add new component");
+	add_component_menu->get_popup()->connect("id_pressed", callable_mp(this, &EntityEditor::_add_component_pressed));
 	add_child(add_component_menu);
 
 	components_section = memnew(EditorInspectorSection);
@@ -43,9 +44,7 @@ void EntityEditor::update_editors() {
 
 	if (add_component_menu) {
 		// Remove all old components.
-		for (int i = 0; i < add_component_menu->get_popup()->get_item_count(); i += 1) {
-			add_component_menu->get_popup()->remove_item(i);
-		}
+		add_component_menu->get_popup()->clear();
 
 		const LocalVector<StringName> &components = ECS::get_registered_components();
 		for (uint32_t i = 0; i < components.size(); i += 1) {
@@ -55,16 +54,25 @@ void EntityEditor::update_editors() {
 
 	if (components_section) {
 		// Remove old childs.
-		for (int i = 0; i < components_section->get_vbox()->get_child_count(); i += 1) {
-			components_section->get_vbox()->remove_child(components_section->get_vbox()->get_child(i));
+		for (int i = components_section->get_vbox()->get_child_count() - 1; i >= 0; i -= 1) {
+			Node *n = components_section->get_vbox()->get_child(i);
+			components_section->get_vbox()->remove_child(n);
+			memdelete(n);
 		}
 
-		const LocalVector<StringName> &components = entity->get_components();
-		for (uint32_t i = 0; i < components.size(); i += 1) {
+		const Dictionary &components = entity->get_components_data();
+		for (const Variant *key = components.next(nullptr); key != nullptr; key = components.next(key)) {
+			const Variant *data = components.getptr(*key);
 			// Add the components of this Entity
 			EditorInspectorSection *component_section = memnew(EditorInspectorSection);
-			component_section->setup("component_" + components[i], components[i], entity, section_color, true);
+			component_section->setup("component_" + String(*key), String(*key), entity, section_color, true);
 			component_section->unfold();
+
+			Button *del_btn = memnew(Button);
+			del_btn->set_text("Remove component");
+			del_btn->set_flat(true);
+			del_btn->connect("pressed", callable_mp(this, &EntityEditor::_remove_component_pressed), varray(key->operator StringName()));
+			component_section->get_vbox()->add_child(del_btn);
 
 			// TODO here the component properties.
 			Label *lbl = memnew(Label);
@@ -74,6 +82,16 @@ void EntityEditor::update_editors() {
 			components_section->get_vbox()->add_child(component_section);
 		}
 	}
+}
+
+void EntityEditor::_add_component_pressed(uint32_t p_component_id) {
+	entity->add_component_data(ECS::get_registered_components()[p_component_id]);
+	update_editors();
+}
+
+void EntityEditor::_remove_component_pressed(StringName p_component_name) {
+	entity->remove_component_data(p_component_name);
+	update_editors();
 }
 
 bool EditorInspectorPluginEntity::can_handle(Object *p_object) {
