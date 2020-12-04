93daa5749fe84ae5c603f7cff5aff4902c2ac89e
AndreaCatania <info@andreacatania.com>
2020-12-04 18:31:19 +0100

    Adding ECS panel control

--- modules/ecs/editor_plugins/editor_world_ecs.cpp
+++ modules/ecs/editor_plugins/editor_world_ecs.cpp
@@ -5,38 +5,290 @@
 #include "editor/editor_node.h"
 #include "editor/editor_scale.h"
 #include "modules/ecs/nodes/ecs_world.h"
+#include "scene/gui/color_rect.h"
+
+SystemInfoBox::SystemInfoBox(EditorNode *p_editor) :
+		editor(p_editor) {
+	add_theme_constant_override("margin_right", 2);
+	add_theme_constant_override("margin_top", 2);
+	add_theme_constant_override("margin_left", 2);
+	add_theme_constant_override("margin_bottom", 2);
+
+	ColorRect *bg = memnew(ColorRect);
+	bg->set_h_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
+	bg->set_v_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
+	bg->set_color(Color(0.0, 0.0, 0.0, 0.2));
+	add_child(bg);
+
+	MarginContainer *inner_container = memnew(MarginContainer);
+	inner_container->add_theme_constant_override("margin_right", 2);
+	inner_container->add_theme_constant_override("margin_top", 2);
+	inner_container->add_theme_constant_override("margin_left", 20);
+	inner_container->add_theme_constant_override("margin_bottom", 2);
+	add_child(inner_container);
+
+	HBoxContainer *box = memnew(HBoxContainer);
+	box->set_h_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
+	box->set_v_size_flags(0);
+	inner_container->add_child(box);
+
+	system_name = memnew(Label);
+	system_name->set_h_size_flags(0);
+	system_name->set_v_size_flags(0);
+	box->add_child(system_name);
+
+	system_data_list = memnew(ItemList);
+	system_data_list->set_h_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
+	system_data_list->set_v_size_flags(SizeFlags::SIZE_EXPAND);
+	system_data_list->set_auto_height(true);
+	system_data_list->set_max_columns(0);
+	system_data_list->set_fixed_icon_size(Size2(13.0, 13.0));
+	system_data_list->add_theme_constant_override("hseparation", 7.0);
+	box->add_child(system_data_list);
+}
+
+SystemInfoBox::SystemInfoBox(EditorNode *p_editor, const String &p_system_name) :
+		SystemInfoBox(p_editor) {
+	set_system_name(p_system_name);
+}
+
+void SystemInfoBox::set_system_name(const String &p_name) {
+	system_name->set_text(p_name);
+}
+
+void SystemInfoBox::add_component(const String &p_name, bool is_write) {
+	Ref<Texture2D> icon;
+	if (is_write) {
+		icon = editor->get_theme_base()->get_theme_icon("Edit", "EditorIcons");
+	} else {
+		icon = editor->get_theme_base()->get_theme_icon("GuiVisibilityVisible", "EditorIcons");
+	}
+	system_data_list->add_item(p_name, icon, false);
+}
+
+Point2 SystemInfoBox::name_global_transform() const {
+	return system_name->get_global_position() + Vector2(0.0, system_name->get_size().y / 2.0);
+}
+
+DrawLayer::DrawLayer() {
+	// This is just a draw layer, we don't need input.
+	set_mouse_filter(MOUSE_FILTER_IGNORE);
+}
+
+void DrawLayer::_notification(int p_what) {
+	switch (p_what) {
+		case NOTIFICATION_INTERNAL_PROCESS: {
+			if (editor->is_ui_dirty) {
+				editor->is_ui_dirty = false;
+				update();
+			}
+		} break;
+		case NOTIFICATION_DRAW: {
+			editor->draw(this);
+		} break;
+		case NOTIFICATION_READY: {
+			set_process_internal(true);
+		} break;
+	}
+}
 
 EditorWorldECS::EditorWorldECS() {
-	Ref<StyleBoxEmpty> border;
-	border.instance();
-	border->set_default_margin(MARGIN_LEFT, 5 * EDSCALE);
-	border->set_default_margin(MARGIN_RIGHT, 5 * EDSCALE);
-	border->set_default_margin(MARGIN_BOTTOM, 5 * EDSCALE);
-	border->set_default_margin(MARGIN_TOP, 5 * EDSCALE);
-	add_theme_style_override("panel", border);
-
 	HBoxContainer *main_hb = memnew(HBoxContainer);
-	main_hb->set_anchor(Margin::MARGIN_LEFT, 0.0);
-	main_hb->set_anchor(Margin::MARGIN_TOP, 0.0);
-	main_hb->set_anchor(Margin::MARGIN_RIGHT, 1.0);
-	main_hb->set_anchor(Margin::MARGIN_BOTTOM, 1.0);
-	main_hb->set_v_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
 	main_hb->set_h_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
 	main_hb->set_v_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
+	main_hb->set_anchor(MARGIN_LEFT, 0.0);
+	main_hb->set_anchor(MARGIN_TOP, 0.0);
+	main_hb->set_anchor(MARGIN_RIGHT, 1.0);
+	main_hb->set_anchor(MARGIN_BOTTOM, 1.0);
 	add_child(main_hb);
 
-	Label *lbl = memnew(Label);
-	lbl->set_text("aa");
-	add_child(lbl);
+	DrawLayer *draw_layer = memnew(DrawLayer);
+	draw_layer->editor = this;
+	draw_layer->set_h_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
+	draw_layer->set_v_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
+	draw_layer->set_anchor(MARGIN_LEFT, 0.0);
+	draw_layer->set_anchor(MARGIN_TOP, 0.0);
+	draw_layer->set_anchor(MARGIN_RIGHT, 1.0);
+	draw_layer->set_anchor(MARGIN_BOTTOM, 1.0);
+	draw_layer->set_margin(MARGIN_LEFT, 0.0);
+	draw_layer->set_margin(MARGIN_TOP, 0.0);
+	draw_layer->set_margin(MARGIN_RIGHT, 0.0);
+	draw_layer->set_margin(MARGIN_BOTTOM, 0.0);
+	add_child(draw_layer);
 
-	lbl = memnew(Label);
-	lbl->set_text("bb");
-	add_child(lbl);
+	// ~~ Left Panel ~~
+	{
+		VBoxContainer *main_container = memnew(VBoxContainer);
+		main_container->set_h_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
+		main_container->set_v_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
+		main_hb->add_child(main_container);
+
+		// ~~ Info box ~~
+		{
+			HBoxContainer *title_box = memnew(HBoxContainer);
+			title_box->set_h_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
+			title_box->set_v_size_flags(0);
+			main_container->add_child(title_box);
+
+			Label *title = memnew(Label);
+			title->set_text(TTR("ECS Node:"));
+			title->set_valign(Label::VALIGN_CENTER);
+			title->set_h_size_flags(0);
+			title->set_v_size_flags(0);
+			title_box->add_child(title);
+
+			node_name_lbl = memnew(Label);
+			node_name_lbl->set_valign(Label::VALIGN_CENTER);
+			node_name_lbl->set_h_size_flags(0);
+			node_name_lbl->set_v_size_flags(0);
+			node_name_lbl->add_theme_color_override("font_color", Color(0.0, 0.5, 1.0));
+			title_box->add_child(node_name_lbl);
+
+			// TODO populate with other info here.
+		}
+
+		// ~~ Resources box ~~
+		{
+			Label *title = memnew(Label);
+			title->set_text(TTR("Resources"));
+			title->set_valign(Label::VALIGN_CENTER);
+			title->set_h_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
+			title->set_v_size_flags(0);
+			main_container->add_child(title);
+		}
+	}
+
+	VSeparator *separator = memnew(VSeparator);
+	main_hb->add_child(separator);
+
+	// ~~ Right Panel ~~
+	{
+		VBoxContainer *main_container = memnew(VBoxContainer);
+		main_container->set_h_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
+		main_container->set_v_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
+		main_hb->add_child(main_container);
+
+		Label *title = memnew(Label);
+		title->set_text(TTR("Pipeline"));
+		title->set_valign(Label::VALIGN_CENTER);
+		title->set_h_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
+		title->set_v_size_flags(0);
+		main_container->add_child(title);
+
+		Panel *panel = memnew(Panel);
+		panel->set_h_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
+		panel->set_v_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
+		panel->set_anchor(MARGIN_LEFT, 0.0);
+		panel->set_anchor(MARGIN_TOP, 0.0);
+		panel->set_anchor(MARGIN_RIGHT, 1.0);
+		panel->set_anchor(MARGIN_BOTTOM, 1.0);
+		main_container->add_child(panel);
+
+		ScrollContainer *wrapper = memnew(ScrollContainer);
+		wrapper->set_h_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
+		wrapper->set_v_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
+		wrapper->set_anchor(MARGIN_LEFT, 0.0);
+		wrapper->set_anchor(MARGIN_TOP, 0.0);
+		wrapper->set_anchor(MARGIN_RIGHT, 1.0);
+		wrapper->set_anchor(MARGIN_BOTTOM, 1.0);
+		wrapper->set_enable_h_scroll(true);
+		wrapper->set_enable_v_scroll(false);
+		panel->add_child(wrapper);
+
+		pipeline_panel = memnew(VBoxContainer);
+		pipeline_panel->set_h_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
+		pipeline_panel->set_v_size_flags(SizeFlags::SIZE_FILL | SizeFlags::SIZE_EXPAND);
+		wrapper->add_child(pipeline_panel);
+
+		Button *add_system_btn = memnew(Button);
+		add_system_btn->set_text(TTR("Add System"));
+		main_container->add_child(add_system_btn);
+	}
+}
+
+void EditorWorldECS::_notification(int p_what) {
+}
+
+void EditorWorldECS::set_world_ecs(WorldECS *p_world) {
+	world_ecs = p_world;
+	pipeline_clear();
+
+	if (world_ecs != nullptr) {
+		node_name_lbl->set_text(world_ecs->get_name());
+
+		// TODO try SystemInfo.
+		pipeline_add_system(memnew(SystemInfoBox(editor, "System 1")));
+		pipeline_add_system(memnew(SystemInfoBox(editor, "System 2")));
+		pipeline_add_system(memnew(SystemInfoBox(editor, "System 3")));
+		pipeline_add_system(memnew(SystemInfoBox(editor, "System 4")));
+
+		is_ui_dirty = true;
+
+	} else {
+		node_name_lbl->set_text("");
+	}
+}
+
+void EditorWorldECS::draw(DrawLayer *p_draw_layer) {
+	if (pipeline_systems.size() <= 0) {
+		return;
+	}
+
+	// Setting this value here, so I can avoid to pass this pointer to each func.
+	draw_layer = p_draw_layer;
+
+	pipeline_draw_batch(0, 1);
+	pipeline_draw_batch(1, 3);
+
+	draw_layer = nullptr;
+}
+
+void EditorWorldECS::pipeline_add_system(SystemInfoBox *p_system) {
+	p_system->add_component("Test write", true);
+	p_system->add_component("Test read", false);
+	pipeline_systems.push_back(p_system);
+	pipeline_panel->add_child(p_system);
+}
+
+void EditorWorldECS::pipeline_clear() {
+	for (int i = pipeline_panel->get_child_count() - 1; i >= 0; i -= 1) {
+		Node *n = pipeline_panel->get_child(i);
+		pipeline_panel->get_child(i)->remove_and_skip();
+		memdelete(n);
+	}
+	pipeline_systems.clear();
+}
+
+void EditorWorldECS::pipeline_draw_batch(uint32_t p_start_system, uint32_t p_end_system) {
+	ERR_FAIL_COND(p_start_system > p_end_system);
+	ERR_FAIL_COND(p_end_system >= pipeline_systems.size());
+
+	const Point2 this_pos = draw_layer->get_global_position();
+	const Point2 point_offset(-10.0, 10.0);
+	const Point2 circle_offset(5.0, 0.0);
+
+	Point2 prev;
+
+	// Draw the points
+	for (uint32_t i = p_start_system; i <= p_end_system; i += 1) {
+		const Point2 current_point = pipeline_systems[i]->name_global_transform() - this_pos + point_offset;
+		if (i != p_start_system) {
+			draw_layer->draw_line(prev, current_point, Color(1.0, 1.0, 1.0, 0.4), 2.0);
+		}
+
+		draw_layer->draw_circle(
+				current_point + circle_offset,
+				4.0,
+				Color(1.0, 1.0, 1.0, 0.4));
+
+		prev = current_point;
+	}
 }
 
 WorldECSEditorPlugin::WorldECSEditorPlugin(EditorNode *p_node) :
 		editor(p_node) {
 	ecs_editor = memnew(EditorWorldECS);
+	ecs_editor->editor = p_node;
 	ecs_editor->set_v_size_flags(Control::SIZE_EXPAND_FILL);
 	editor->get_viewport()->add_child(ecs_editor);
 	ecs_editor->set_anchors_and_margins_preset(Control::PRESET_WIDE);
@@ -52,6 +304,7 @@ WorldECSEditorPlugin::~WorldECSEditorPlugin() {
 void WorldECSEditorPlugin::edit(Object *p_object) {
 	world_ecs = Object::cast_to<WorldECS>(p_object);
 	ERR_FAIL_COND_MSG(world_ecs == nullptr, "The object should be of type WorldECS [BUG].");
+	ecs_editor->set_world_ecs(world_ecs);
 }
 
 bool WorldECSEditorPlugin::handles(Object *p_object) const {
@@ -63,5 +316,6 @@ void WorldECSEditorPlugin::make_visible(bool p_visible) {
 		ecs_editor->show();
 	} else {
 		ecs_editor->hide();
+		ecs_editor->set_world_ecs(nullptr);
 	}
 }
--- modules/ecs/editor_plugins/editor_world_ecs.h
+++ modules/ecs/editor_plugins/editor_world_ecs.h
@@ -7,20 +7,78 @@
 
 class EditorNode;
 class WorldECS;
+class EditorWorldECS;
+
+class SystemInfoBox : public MarginContainer {
+	GDCLASS(SystemInfoBox, MarginContainer);
+
+	EditorNode *editor = nullptr;
+	Label *system_name = nullptr;
+	ItemList *system_data_list = nullptr;
+
+public:
+	SystemInfoBox(EditorNode *p_editor);
+	SystemInfoBox(EditorNode *p_ditor, const String &p_system_name);
+
+	void set_system_name(const String &p_name);
+	void add_component(const String &p_name, bool is_write);
+
+	Point2 name_global_transform() const;
+};
+
+class DrawLayer : public Control {
+	GDCLASS(DrawLayer, Control);
+
+public:
+	EditorWorldECS *editor = nullptr;
+
+public:
+	DrawLayer();
+
+	void _notification(int p_what);
+};
 
 class EditorWorldECS : public PanelContainer {
 	GDCLASS(EditorWorldECS, PanelContainer);
 
+	friend class DrawLayer;
+	friend class WorldECSEditorPlugin;
+
+	EditorNode *editor = nullptr;
+	WorldECS *world_ecs = nullptr;
+
+	DrawLayer *draw_layer = nullptr;
+	Label *node_name_lbl = nullptr;
+	VBoxContainer *pipeline_panel = nullptr;
+	LocalVector<SystemInfoBox *> pipeline_systems;
+
+	bool is_ui_dirty = false;
+
 public:
 	EditorWorldECS();
+
+	void _notification(int p_what);
+
+	void set_world_ecs(WorldECS *p_world);
+
+	void draw(DrawLayer *p_draw_layer);
+
+	void pipeline_add_system(SystemInfoBox *p_system);
+	void pipeline_clear();
+
+	void pipeline_draw_batch(uint32_t p_start_system, uint32_t p_end_system);
 };
 
 class WorldECSEditorPlugin : public EditorPlugin {
 	GDCLASS(WorldECSEditorPlugin, EditorPlugin);
 
+	friend class SystemInfoBox;
+	friend class DrawLayer;
+	friend class EditorWorldECS;
+
 	EditorNode *editor = nullptr;
 	EditorWorldECS *ecs_editor = nullptr;
-	WorldECS *world_ecs;
+	WorldECS *world_ecs = nullptr;
 
 public:
 	WorldECSEditorPlugin(EditorNode *p_node);
